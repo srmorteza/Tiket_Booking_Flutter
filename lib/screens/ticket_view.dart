bc6739006706d742e63db9b9306f8809f062b30a
sr.morteza <sr.morteza@gmail.com>
2022-11-03 14:04:31 +0330

    ADD SOME CODE IN HOMEPAGE 2 1401-8-12

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,16 +1,18 @@
 import 'package:booktickets/utils/app_layout.dart';
 import 'package:booktickets/utils/app_styles.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import '../widgets/thick_container.dart';
+import 'package:flutter/src/widgets/layout_builder.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final size = Applayout.getSize(context) ;
+    final size = Applayout.getSize(context);
     return SizedBox(
-      width: size.width ,
+      width: size.width,
       height: 200,
       child: Container(
         margin: const EdgeInsets.only(left: 16),
@@ -19,16 +21,76 @@ class TicketView extends StatelessWidget {
             Container(
               decoration: const BoxDecoration(
                 color: Color(0xff526799),
-                borderRadius:BorderRadius.only(topLeft: Radius.circular(21),topRight: Radius.circular(21)),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(21),
+                    topRight: Radius.circular(21)),
               ),
               padding: const EdgeInsets.all(16),
               child: Column(
                 children: [
                   Row(
                     children: [
-                      Text("NYC" ,style: Styles.headLineStyle3.copyWith(color: Colors.white),),
+                      Text(
+                        "NYC",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      ),
                       const Spacer(),
-                      Text("London" ,style: Styles.headLineStyle3.copyWith(color: Colors.white),),
+                      ThickContainer(),
+                      Expanded(
+                          child: Stack(
+                            children: [SizedBox(
+                                height: 24,
+                                child: LayoutBuilder(
+                                  builder: (BuildContext context,
+                                      BoxConstraints constraints) {
+                                    return Flex(
+                                      direction: Axis.horizontal,
+                                      mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: List.generate(
+                                          (constraints.constrainWidth() / 6)
+                                              .floor(),
+                                              (index) => SizedBox(
+                                            width: 3,
+                                            height: 1,
+                                            child: DecoratedBox(
+                                              decoration: BoxDecoration(
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          )),
+                                    );
+                                  },
+                                )),
+                              Center(child: Transform.rotate(angle: 1.5, child: Icon(Icons.local_airport_rounded , color: Colors.white,),)),
+                            ],
+                          )),
+
+                      ThickContainer(),
+                      const Spacer(),
+                      Text(
+                        "LDN",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  const Gap(3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text("New-york", style: Styles.headLineStyle4.copyWith(color: Colors.white),),
+                      ),
+                      Text("8H 30M",style: Styles.headLineStyle4.copyWith(color: Colors.white),),
+                      SizedBox(
+                        width: 100,
+                        child: Text("London", style: Styles.headLineStyle4.copyWith(color: Colors.white),
+                        ),
+                      ),
                     ],
                   )
                 ],
