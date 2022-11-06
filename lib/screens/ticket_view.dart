e13c17d40145cc0b6344efb097c526ecf5c0465a
sr.morteza <sr.morteza@gmail.com>
2022-11-06 13:36:49 +0330

    add scroll to main page and complete the ticket slider 1401-8-15

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -12,10 +12,10 @@ class TicketView extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = Applayout.getSize(context);
     return SizedBox(
-      width: size.width,
+      width: size.width*0.85,
       height: 200,
       child: Container(
-        margin: const EdgeInsets.only(left: 16),
+        margin: const EdgeInsets.only(right: 16),
         child: Column(
           children: [
             Container(
@@ -67,7 +67,6 @@ class TicketView extends StatelessWidget {
                               Center(child: Transform.rotate(angle: 1.5, child: Icon(Icons.local_airport_rounded , color: Colors.white,),)),
                             ],
                           )),
-
                       ThickContainer(),
                       const Spacer(),
                       Text(
@@ -77,22 +76,116 @@ class TicketView extends StatelessWidget {
                       ),
                     ],
                   ),
-                  const Gap(3),
+                  const Gap(5),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       SizedBox(
                         width: 100,
                         child: Text("New-york", style: Styles.headLineStyle4.copyWith(color: Colors.white),),
                       ),
                       Text("8H 30M",style: Styles.headLineStyle4.copyWith(color: Colors.white),),
+
                       SizedBox(
+
                         width: 100,
                         child: Text("London", style: Styles.headLineStyle4.copyWith(color: Colors.white),
                         ),
                       ),
                     ],
                   )
+                ],
+              ),
+            ),
+            Container(
+              color: Styles.orangeColor,
+              child: Row(
+                children:  [
+                  SizedBox(
+
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+
+                        )
+                      ),
+                    ),
+                  ),
+                 Expanded(child: LayoutBuilder(
+                     builder: ( BuildContext context ,BoxConstraints constraints){
+                       return Flex(
+                         direction: Axis.horizontal,
+                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                         mainAxisSize: MainAxisSize.max,
+                         children: List.generate((constraints.constrainWidth()/15).floor(), (index) => const SizedBox(
+                           width: 5,
+                           height: 1,
+                           child: DecoratedBox(
+                             decoration: BoxDecoration(
+                               color: Colors.white,
+                             ),
+                           ),
+                         )),
+                       );
+
+                 })),
+                  SizedBox(
+
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+
+                          )
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 16, top: 10,right: 16, bottom: 10),
+              decoration: BoxDecoration(
+                color: Styles.orangeColor,
+                borderRadius: BorderRadius.only(bottomRight: Radius.circular(21) , bottomLeft: Radius.circular(21)),
+                ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("1 MAY" , style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+                        Text("DATE" , style: Styles.headLineStyle4.copyWith(color: Colors.white)),
+                      ],
+                    ) ,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text("8:00 AM" , style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+                          Text("Departure Time" , style: Styles.headLineStyle4.copyWith(color: Colors.white)),
+                        ],
+                      ) ,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text("23" , style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+                        Text("Number" , style: Styles.headLineStyle4.copyWith(color: Colors.white)),
+                      ],
+                    ) ,
+                  ],)
                 ],
               ),
             )
--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -6,7 +6,7 @@ class Styles{
   static Color primeryColor= primery ;
   static Color textColor = const Color(0xff3b3b3b) ;
   static Color bgColor = const Color(0xffeeedf2) ;
-  static Color orangeColor = const Color(0xff526799) ;
+  static Color orangeColor = const Color(0xfff37b67) ;
   static TextStyle  textStyle = TextStyle(fontSize:16, color: textColor, fontWeight: FontWeight.w500);
   static TextStyle  headLineStyle1 = TextStyle(fontSize: 26, color: textColor, fontWeight: FontWeight.bold);
   static TextStyle  headLineStyle2 = TextStyle(fontSize: 21, color: textColor, fontWeight: FontWeight.bold);
