0084a818e878eee35185319e125a40d6d9c1b014
sr.morteza <sr.morteza@gmail.com>
2022-11-09 15:34:09 +0330

    refactor some code and make some component in ticket view page 1401-8-18

--- lib/screens/ticket_screen.dart
+++ lib/screens/ticket_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 import '../utils/app_layout.dart';
+import '../widgets/column_layout.dart';
 
 class TicketScreen extends StatelessWidget {
   const TicketScreen({Key? key}) : super(key: key);
@@ -15,6 +16,7 @@ class TicketScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = Applayout.getSize(context);
     return Scaffold(
+      backgroundColor: Styles.bgColor,
       body: Stack(
         children: [
         ListView(
@@ -26,8 +28,27 @@ class TicketScreen extends StatelessWidget {
           const AppTicketTabs(firsttab: "Upcoming", secondtab: "Previos"),
           Gap(Applayout.getHeight(25)),
           Container(
-            padding: EdgeInsets.only(left:Applayout.getHeight(25) ),
+            padding: EdgeInsets.only(left:Applayout.getHeight(16) ),
             child: TicketView(ticket: ticketList[0],isColor: true),
+          ),
+          SizedBox(height: 1,),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+            margin: EdgeInsets.only(right: Applayout.getHeight(16) ,left:Applayout.getWidht(16)),
+            color: Colors.white,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment:  MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppColumnLayout(firstText: "Flutter DB",secondText: "passenger",alignment: CrossAxisAlignment.start,isColor:false),
+                    AppColumnLayout(firstText: "5221 364869",secondText: "passport",alignment: CrossAxisAlignment.end,isColor:false),
+
+
+                  ],
+                )
+              ],
+            ),
           )
           
         ],
--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -2,6 +2,9 @@ import 'package:booktickets/utils/app_layout.dart';
 import 'package:booktickets/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import '../widgets/column_layout.dart';
+import '../widgets/layout_builder_widget.dart';
 import '../widgets/thick_container.dart';
 import 'package:flutter/src/widgets/layout_builder.dart';
 
@@ -15,7 +18,7 @@ class TicketView extends StatelessWidget {
     final size = Applayout.getSize(context);
     return SizedBox(
       width: size.width*0.85,
-      height: Applayout.getHeight(200),
+      height: Applayout.getHeight(GetPlatform.isAndroid==true ?153:169),
       child: Container(
         margin: EdgeInsets.only(right: Applayout.getHeight(16)),
         child: Column(
@@ -43,31 +46,11 @@ class TicketView extends StatelessWidget {
                           child: Stack(
                             children: [SizedBox(
                                 height: Applayout.getHeight(24),
-                                child: LayoutBuilder(
-                                  builder: (BuildContext context,
-                                      BoxConstraints constraints) {
-                                    return Flex(
-                                      direction: Axis.horizontal,
-                                      mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                      mainAxisSize: MainAxisSize.max,
-                                      children: List.generate(
-                                          (constraints.constrainWidth() / 6)
-                                              .floor(),
-                                              (index) => SizedBox(
-                                            width: 3,
-                                            height: 1,
-                                            child: DecoratedBox(
-                                              decoration: BoxDecoration(
-                                                color: Colors.white,
-                                              ),
-                                            ),
-                                          )),
-                                    );
-                                  },
-                                )),
-                              Center(child: Transform.rotate(angle: 1.5, child: Icon(Icons.local_airport_rounded , color: isColor == null ? Colors.white: Color(0xff8accf7),),)),
-                            ],
+                                child:
+                                AppLayoutBuilderWidget(sections: 6),),
+                                Center(child: Transform.rotate(angle: 1.5, child: Icon(Icons.local_airport_rounded , color: isColor == null ? Colors.white: Color(0xff8accf7),),)),
+
+                        ],
                           )),
                       ThickContainer(isColor:true),
                       const Spacer(),
@@ -99,6 +82,7 @@ class TicketView extends StatelessWidget {
                 ],
               ),
             ),
+
             Container(
               color: isColor ==null ?Styles.orangeColor: Colors.white,
               child: Row(
@@ -154,37 +138,21 @@ class TicketView extends StatelessWidget {
               ),
             ),
             Container(
-              padding: const EdgeInsets.only(left: 16, top: 10,right: 16, bottom: 10),
+              padding: const EdgeInsets.only(left: 16, top: 10,right: 16, bottom: 8),
               decoration: BoxDecoration(
                 color: isColor == null ? Styles.orangeColor :Colors.white,
-                borderRadius: BorderRadius.only(bottomRight: Radius.circular(21) , bottomLeft: Radius.circular(21)),
+                borderRadius: BorderRadius.only(bottomRight: Radius.circular(isColor==null?21:0) , bottomLeft: Radius.circular(isColor==null?21:0)),
                 ),
               child: Column(
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(ticket['date'] , style: isColor == null ? Styles.headLineStyle3.copyWith(color: Colors.white):Styles.headLineStyle3),
-                        Text("DATE" , style: isColor == null ? Styles.headLineStyle4.copyWith(color: Colors.white):Styles.headLineStyle4),
-                      ],
-                    ) ,
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(ticket['departure_time'], style:  isColor == null ? Styles.headLineStyle3.copyWith(color: Colors.white):Styles.headLineStyle3),
-                          Text("Departure Time" , style:  isColor == null ?Styles.headLineStyle4.copyWith(color: Colors.white):Styles.headLineStyle4),
-                        ],
-                      ) ,
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(ticket['number'].toString() , style:  isColor == null ? Styles.headLineStyle3.copyWith(color: Colors.white):Styles.headLineStyle3),
-                        Text("Number" , style:  isColor == null ? Styles.headLineStyle4.copyWith(color: Colors.white):Styles.headLineStyle4),
-                      ],
-                    ) ,
+                      AppColumnLayout(firstText: "DATE",secondText:ticket['date'] ,alignment:CrossAxisAlignment.start ,isColor: false,),
+                      AppColumnLayout(firstText: ticket['departure_time'],secondText:"Departure Time"  ,alignment: CrossAxisAlignment.center,isColor: false,),
+                      AppColumnLayout(firstText: ticket['number'].toString(),secondText:"Number",alignment:CrossAxisAlignment.end, isColor: false,),
+
+
                   ],)
                 ],
               ),
